c609b5910c9643e48b76711a20ad2d8180d42d40
Tarek Kalaji <tarek-aec@hotmail.com>
2016-09-02 12:39:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+from lib.test import TestCase
+if __name__ == '__main__':
+	test = TestCase()
+	#test.send_one_message_to_many_receipients()
+	#test.send_one_message_to_many_receipients_schedule_delivery()
+	#test.send_one_message_to_many_receipients_schedule_delivery_with_validity_period()
+	#test.send_one_message_to_many_receipients_turkish_language()
+	#test.send_one_message_to_many_receipients_flash_sms()
+	#test.send_one_message_to_many_receipients_unicode()
+	#test.send_one_message_to_many_receipients_outside_turkey()
+	#test.send_many_message_to_many_receipients()
+	#test.query_multi_general_report()
+	#test.query_multi_general_report_with_id()
+	#test.query_detailed_report_with_id()
+	#test.get_account_settings()
+	#test.get_authority()
+	#test.get_cdr_report()
+	#test.get_cdr_report_range_datetime()
+	#test.get_cdr_report_with_type()
+	#test.get_active_calls()
+	#test.get_disconnect_active_call()
